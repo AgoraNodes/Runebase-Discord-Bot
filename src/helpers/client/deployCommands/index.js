1116ca7ab0823e819854faff906b29004df5cd60
bobpepers <bob.pepers@gmail.com>
2022-06-05 07:58:34 +0200

    add command registration

--- src/helpers/client/deployCommands/index.js
+++ src/helpers/client/deployCommands/index.js
@@ -2,22 +2,25 @@ import { config } from "dotenv";
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
+import db from '../../../models';
 
 config();
 
 const commands = [
   new SlashCommandBuilder().setName('help').setDescription('DM\'s you with a help message'),
-  new SlashCommandBuilder().setName('deposit').setDescription('Replies with server info!'),
-  new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+  new SlashCommandBuilder().setName('deposit').setDescription('Replies with a deposit address!'),
+  new SlashCommandBuilder().setName('account').setDescription('Replies with account info'),
+  new SlashCommandBuilder().setName('myrank').setDescription('Replies with user\'s rank'),
 ].map((command) => command.toJSON());
 
-export const deployCommands = (
+export const deployCommands = async (
   botToken,
   clientId,
 ) => {
+  const setting = await db.setting.findOne();
   const rest = new REST({ version: '9' }).setToken(botToken);
 
-  rest.put(Routes.applicationGuildCommands(clientId, 'guildId'), { body: commands })
+  rest.put(Routes.applicationGuildCommands(clientId, setting.discordHomeServerGuildId), { body: commands })
     .then(() => console.log('Successfully registered application commands.'))
     .catch(console.error);
 };
