f734003e38fc371b32b9511a7d7d30ebe140d66d
bobpepers <bob.pepers@gmail.com>
2022-06-10 23:13:39 +0200

    leaderboard and most active chat functions

--- src/controllers/myrank.js
+++ src/controllers/myrank.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import {
+  Sequelize,
   Transaction,
   Op,
 } from "sequelize";
@@ -40,6 +41,24 @@ export const discordMyRank = async (
     }
     if (!user) return;
 
+    const totalChatActivity = await db.activeTalker.findOne({
+      attributes: [[Sequelize.fn('sum', Sequelize.col('count')), 'count']],
+      raw: true,
+      where: {
+        userId: user.id,
+      },
+    });
+    const monthlyChatActivity = await db.activeTalker.findOne({
+      attributes: [[Sequelize.fn('sum', Sequelize.col('count')), 'count']],
+      raw: true,
+      where: {
+        userId: user.id,
+        createdAt: {
+          [Op.gt]: new Date(Date.now() - (30 * 24 * 60 * 60 * 1000)),
+        },
+      },
+    });
+
     const currentRank = await db.rank.findOne({
       where: {
         expNeeded: {
@@ -71,7 +90,8 @@ export const discordMyRank = async (
       transaction: t,
       lock: t.LOCK.UPDATE,
     });
-    const nextRankExp = nextRank.expNeeded;
+
+    const nextRankExp = nextRank && nextRank.expNeeded ? nextRank.expNeeded : currentRankExp;
     const currentExp = user.exp;
     await registerFont(path.join(__dirname, '../assets/fonts/', 'Heart_warming.otf'), { family: 'HeartWarming' });
     const canvas = createCanvas(1000, 300);
@@ -86,7 +106,6 @@ export const discordMyRank = async (
     } else {
       avatar = await loadImage(`https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png?size=256`);
     }
-
     // background
     // ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
@@ -98,27 +117,35 @@ export const discordMyRank = async (
 
     ctx.strokeStyle = "#164179";
     ctx.fill();
-    // ctx.stroke();
     ctx.closePath();
 
     // XP Bar
     ctx.lineJoin = 'round';
     ctx.lineWidth = 69;
     ctx.strokeStyle = "#164179";
+
     // shadow of xp bar
-    ctx.strokeRect(298, 199, expBarWidth, 2);
+    ctx.strokeRect(323, 239, expBarWidth, 2);
 
     // empty bar
     ctx.strokeStyle = 'black';
-    ctx.strokeRect(300, 200, expBarWidth, 0);
+    ctx.strokeRect(325, 240, expBarWidth, 0);
 
     // filled bar
     const reqExp = nextRankExp - currentRankExp;
     const calculatedCurrentExp = currentExp - currentRankExp;
-    const percentage = (calculatedCurrentExp / reqExp) * 100;
+    let percentage = (calculatedCurrentExp / reqExp) * 100;
+    if (percentage === Infinity) {
+      percentage = (currentExp / nextRankExp) * 100;
+    }
 
     ctx.strokeStyle = '#348128';
-    ctx.strokeRect(300, 200, expBarWidth * (calculatedCurrentExp / reqExp), 0);
+    ctx.strokeRect(
+      323,
+      240,
+      percentage < 100 ? expBarWidth * (calculatedCurrentExp / reqExp) : expBarWidth,
+      0,
+    );
 
     // Adding text
     ctx.font = 'bold 40px "HeartWarming"';
@@ -129,38 +156,72 @@ export const discordMyRank = async (
     ctx.strokeText(user.username, 120, 275, 200);
     ctx.fillText(user.username, 120, 275, 200);
 
-    ctx.strokeText(currentRank ? currentRank.name : 'Unranked', 722, 80, 100);
-    ctx.fillText(currentRank ? currentRank.name : 'Unranked', 722, 80, 100);
-    ctx.strokeText(`${currentRank ? currentRank.id : 0}`, 930, 80, 80);
-    ctx.fillText(`${currentRank ? currentRank.id : 0}`, 930, 80, 80);
+    ctx.strokeText(currentRank ? currentRank.name : 'Unranked', 722, 90, 100);
+    ctx.fillText(currentRank ? currentRank.name : 'Unranked', 722, 90, 100);
+    ctx.strokeText(`${currentRank ? currentRank.id : 0}`, 900, 90, 80);
+    ctx.fillText(`${currentRank ? currentRank.id : 0}`, 900, 90, 80);
 
     ctx.fillStyle = 'white';
     ctx.font = 'bold 25px "HeartWarming"';
-    ctx.strokeText("Rank", 720, 40, 200);
-    ctx.fillText("Rank", 720, 40, 200);
-    ctx.strokeText("Level", 930, 40, 200);
-    ctx.fillText("Level", 930, 40, 200);
-    ctx.strokeText("Current exp", 610, 120, 200);
-    ctx.fillText("Current exp", 610, 120, 200);
-    ctx.strokeText("prev", 320, 120, 200);
-    ctx.fillText("prev", 320, 120, 200);
-    ctx.strokeText("next", 880, 120, 200);
-    ctx.fillText("next", 880, 120, 200);
+    ctx.strokeText("Chat Activity Score", 450, 40, 300);
+    ctx.fillText("Chat Activity Score", 450, 40, 300);
+    ctx.strokeText("Rank", 720, 50, 200);
+    ctx.fillText("Rank", 720, 50, 200);
+    ctx.strokeText("Level", 900, 50, 200);
+    ctx.fillText("Level", 900, 50, 200);
+    ctx.strokeText("Current exp", 635, 160, 200);
+    ctx.fillText("Current exp", 635, 160, 200);
+    ctx.strokeText("prev", 345, 160, 200);
+    ctx.fillText("prev", 345, 160, 200);
+    ctx.strokeText("next", 905, 160, 200);
+    ctx.fillText("next", 905, 160, 200);
 
     ctx.font = 'bold 25px "HeartWarming"';
     ctx.fillStyle = "#fe5701";
-    ctx.strokeText(currentRankExp, 320, 150, 200);
-    ctx.fillText(currentRankExp, 320, 150, 200);
-    ctx.strokeText(nextRankExp, 880, 150, 200);
-    ctx.fillText(nextRankExp, 880, 150, 200);
-    ctx.strokeText(currentExp, 610, 150, 200);
-    ctx.fillText(currentExp, 610, 150, 200);
+    ctx.strokeText(currentRankExp, 345, 190, 200);
+    ctx.fillText(currentRankExp, 345, 190, 200);
+    ctx.strokeText(nextRankExp, 905, 190, 200);
+    ctx.fillText(nextRankExp, 905, 190, 200);
+    ctx.strokeText(currentExp, 635, 190, 200);
+    ctx.fillText(currentExp, 635, 190, 200);
+
+    // chat scores
+    ctx.strokeText(
+      monthlyChatActivity ? monthlyChatActivity.count : 0,
+      350,
+      100,
+      200,
+    );
+    ctx.fillText(
+      monthlyChatActivity ? monthlyChatActivity.count : 0,
+      350,
+      100,
+      200,
+    );
+    ctx.strokeText(
+      totalChatActivity ? totalChatActivity.count : 0,
+      550,
+      100,
+      200,
+    );
+    ctx.fillText(
+      totalChatActivity ? totalChatActivity.count : 0,
+      550,
+      100,
+      200,
+    );
+
+    ctx.fillStyle = 'white';
+    ctx.strokeText('30 day', 350, 70, 200);
+    ctx.fillText('30 day', 350, 70, 200);
+
+    ctx.strokeText('Total', 550, 70, 200);
+    ctx.fillText('Total', 550, 70, 200);
 
     ctx.font = 'bold 50px "HeartWarming"';
     ctx.fillStyle = "#fe5701";
-    ctx.strokeText(`${percentage.toFixed(0)}%`, 615, 220, 200);
-    ctx.fillText(`${percentage.toFixed(0)}%`, 615, 220, 200);
-
+    ctx.strokeText(`${percentage.toFixed(0)}%`, 640, 260, 200);
+    ctx.fillText(`${percentage.toFixed(0)}%`, 640, 260, 200);
     // remove corners
     ctx.beginPath();
     ctx.arc(120, 120, 110, 0, 2 * Math.PI);
@@ -171,6 +232,8 @@ export const discordMyRank = async (
     ctx.drawImage(avatar, 10, 10, 220, 220);
 
     const attachment = new MessageAttachment(canvas.toBuffer(), 'rank.png');
+
+    console.log('before send');
 
     if (message.type && message.type === 'APPLICATION_COMMAND') {
       const discordUser = await discordClient.users.cache.get(message.user.id);
@@ -236,7 +299,6 @@ export const discordMyRank = async (
     } catch (e) {
       logger.error(`Error Discord: ${e}`);
     }
-    logger.error(`Error Discord Help Requested by: ${message.author.id}-${message.author.username}#${message.author.discriminator} - ${err}`);
     if (err.code && err.code === 50007) {
       if (message.type && message.type === 'APPLICATION_COMMAND') {
         const discordChannel = await discordClient.channels.cache.get(message.channelId);
