40936155aee7dce5387163d4b589cb7aa76ebe1b
bobpepers <bob.pepers@gmail.com>
2022-06-13 03:49:44 +0200

    build v0.0.9

---
+++ src/models/Condition.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataTypes) => {
+  // 1: The model schema.
+  const modelDefinition = {
+    id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    life: {
+      type: DataTypes.SMALLINT,
+      allowNull: false,
+    },
+    mana: {
+      type: DataTypes.SMALLINT,
+      allowNull: false,
+    },
+    stamina: {
+      type: DataTypes.SMALLINT,
+      allowNull: false,
+    },
+  };
+
+  // 2: The model options.
+  const modelOptions = {
+    freezeTableName: true,
+  };
+
+  // 3: Define the Wallet model.
+  const ConditionModel = sequelize.define('condition', modelDefinition, modelOptions);
+
+  ConditionModel.associate = (model) => {
+    ConditionModel.hasOne(model.UserClass, {
+      as: 'condition',
+      foreignKey: 'conditionId',
+      e,
+    });
+  };
+
+  return ConditionModel;
+};
