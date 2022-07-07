977405ac30ca01f248e5a2360e8e42e48824612b
bobpepers <bob.pepers@gmail.com>
2022-07-07 21:43:20 +0200

    Bonk description

--- src/render/skills/skillDescription.js
+++ src/render/skills/skillDescription.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 import {
   Sequelize,
   Transaction,
@@ -5,37 +6,9 @@ import {
 } from "sequelize";
 import {
   createCanvas,
-  loadImage,
-  registerFont,
 } from 'canvas';
-import path from 'path';
 import _ from 'lodash';
-
-function printAtWordWrap(context, text, x, y, lineHeight, fitWidth) {
-  fitWidth = fitWidth || 0;
-
-  if (fitWidth <= 0) {
-    context.fillText(text, x, y);
-    return;
-  }
-  let words = text.split(' ');
-  let currentLine = 0;
-  let idx = 1;
-  while (words.length > 0 && idx <= words.length) {
-    const str = words.slice(0, idx).join(' ');
-    const w = context.measureText(str).width;
-    if (w > fitWidth) {
-      if (idx == 1) {
-        idx = 2;
-      }
-      context.fillText(words.slice(0, idx - 1).join(' '), x, y + (lineHeight * currentLine));
-      currentLine++;
-      words = words.splice(idx - 1);
-      idx = 1;
-    } else { idx++; }
-  }
-  if (idx > 0) { context.fillText(words.join(' '), x, y + (lineHeight * currentLine)); }
-}
+import skills from './skills.json';
 
 export const renderSkillDescriptionImage = async (
   userCharacter,
@@ -43,34 +16,21 @@ export const renderSkillDescriptionImage = async (
   skillTreeIndex,
   selectedSkill,
 ) => {
-  console.log(selectedSkill);
-  console.log('selectedSkill');
+  const jsonSkillInfo = skills.find((x) => x.name === selectedSkill.name);
+  const userHasSkill = userCharacter.UserClassSkills.find((o) => o.skillId === selectedSkill.id);
+
   const canvas = createCanvas(
-    345,
+    350,
     457,
   );
   const ctx = canvas.getContext('2d');
 
   ctx.font = 'bold 25px "HeartWarming"';
   ctx.fillStyle = "#ccc";
-  // ctx.textAlign = "center";
-  ctx.strokeStyle = '#FFD700';
-  ctx.lineWidth = 10;
-
-  ctx.beginPath();
-  ctx.rect(
-    5,
-    30,
-    335,
-    422,
-  );
-  ctx.stroke();
-
-  console.log(selectedSkill.name);
 
   ctx.font = 'bold 18px "HeartWarming"';
   ctx.textAlign = "center";
-  ctx.shadowBlur = 30;
+  ctx.shadowBlur = 3;
   ctx.shadowColor = "red";
   ctx.beginPath();
   ctx.lineWidth = "3";
@@ -78,46 +38,187 @@ export const renderSkillDescriptionImage = async (
   ctx.strokeText(
     selectedSkill.name,
     canvas.width / 2,
-    70,
+    50,
     345,
   );
   ctx.fillText(
     selectedSkill.name,
     canvas.width / 2,
-    70,
+    50,
     345,
   );
+  ctx.stroke();
 
   ctx.lineWidth = "3";
   ctx.strokeStyle = "black";
   ctx.shadowBlur = 0;
   ctx.shadowColor = "none";
 
-  ctx.strokeText(
-    'skill modifier table goes here',
-    canvas.width / 2,
-    110,
-    345,
-  );
-  ctx.fillText(
-    'skill modifier table goes here',
-    canvas.width / 2,
-    110,
-    345,
-  );
+  if (jsonSkillInfo) {
+    ctx.font = 'bold 10px "HeartWarming"';
+    // Box width
+    const bw = 305;
+    // Box height
+    const bh = (18 * 21);
+    // Padding
+    const p = 2;
+    if (userHasSkill) {
+      ctx.fillStyle = "rgba(16, 12, 131, 0.3)";
+      ctx.fillRect(
+        40,
+        p + 60 + (userHasSkill.points * 18),
+        305,
+        18,
+      );
+    }
 
-  ctx.strokeText(
-    'skill description',
-    canvas.width / 2,
-    150,
-    345,
+    ctx.fillStyle = '#F9DC5C';
+    ctx.fillRect(
+      0,
+      p + 60,
+      43,
+      bh,
+    );
+    ctx.fillRect(
+      40,
+      p + 60,
+      305,
+      18,
+    );
+    const totalColumns = Object.keys(jsonSkillInfo.initial).length;
+    const columnWidth = bw / totalColumns;
+
+    for (let x = 0; x <= bw; x += columnWidth) {
+      ctx.fillStyle = '#F9DC5C';
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = "3";
+      ctx.beginPath();
+      ctx.moveTo(
+        40 + x + p,
+        p + 60,
+      );
+      ctx.lineTo(
+        40 + x + p,
+        bh + p + 60,
+      );
+      ctx.stroke();
+
+      if (x === 0) {
+        ctx.fillStyle = 'white';
+        ctx.lineWidth = "1";
+        ctx.beginPath();
+        ctx.font = 'bold 10px "HeartWarming"';
+        Object.entries(jsonSkillInfo.initial).forEach(([key, value], i) => {
+          ctx.strokeText(
+            key,
+            40 + ((i + 1) * columnWidth) - (columnWidth / 2),
+            p + 74,
+            columnWidth,
+          );
+          ctx.fillText(
+            key,
+            40 + ((i + 1) * columnWidth) - (columnWidth / 2),
+            p + 74,
+            columnWidth,
+          );
+        });
+        ctx.stroke();
+      }
+    }
+
+    for (let x = 0; x <= bh; x += 18) {
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = "3";
+      ctx.beginPath();
+      ctx.moveTo(
+        p,
+        60 + x + p,
+      );
+      ctx.lineTo(
+        345,
+        60 + x + p,
+      );
+      ctx.stroke();
+
+      ctx.fillStyle = 'white';
+      ctx.lineWidth = "1";
+      ctx.beginPath();
+      ctx.font = 'bold 10px "HeartWarming"';
+      ctx.strokeText(
+        'Level',
+        19,
+        p + 74,
+        60,
+      );
+      ctx.fillText(
+        'Level',
+        19,
+        p + 74,
+        60,
+      );
+      ctx.stroke();
+      ctx.beginPath();
+      if (x !== 0 && x <= 360) {
+        ctx.strokeText(
+          (x / 18),
+          19,
+          p + 74 + x,
+          60,
+        );
+        ctx.fillText(
+          (x / 18),
+          19,
+          p + 74 + x,
+          60,
+        );
+
+        Object.entries(jsonSkillInfo.initial).forEach(([key, value], p) => {
+          ctx.strokeText(
+            value + (jsonSkillInfo.next[key] * ((x / 18) - 1)),
+            40 + ((p + 1) * columnWidth) - (columnWidth / 2),
+            p + 74 + x,
+            columnWidth,
+          );
+          ctx.fillText(
+            value + (jsonSkillInfo.next[key] * ((x / 18) - 1)),
+            40 + ((p + 1) * columnWidth) - (columnWidth / 2),
+            p + 74 + x,
+            columnWidth,
+          );
+        });
+      }
+      ctx.stroke();
+    }
+  } else {
+    ctx.beginPath();
+    ctx.strokeText(
+      'skillInfo not found',
+      canvas.width / 2,
+      110,
+      350,
+    );
+    ctx.fillText(
+      'skillInfo not found',
+      canvas.width / 2,
+      110,
+      350,
+    );
+    ctx.stroke();
+  }
+  //
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 2;
+  ctx.stroke();
+
+  ctx.strokeStyle = '#FFD700';
+  ctx.beginPath();
+  ctx.rect(
+    1,
+    30,
+    349,
+    422,
   );
-  ctx.fillText(
-    'skill description',
-    canvas.width / 2,
-    150,
-    345,
-  );
+  ctx.stroke();
 
   const finalImage = await canvas.toBuffer();
   return finalImage;
