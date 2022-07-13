0b2eb562dadeee89e3ea1bf336632a11e26ac3c4
bobpepers <bob.pepers@gmail.com>
2022-07-13 21:12:15 +0200

    comments and cleanup battle processor

--- src/processors/battle/processBattleMove.js
+++ src/processors/battle/processBattleMove.js
@@ -1,36 +1,33 @@
 /* eslint-disable no-restricted-syntax */
 import db from '../../models';
-import { calculateCharacterStats } from '../../helpers/stats/calculateCharacterStats';
+
 import userApplyDebuffAoE from './user/userApplyDebuffAoE';
 import userApplyDebuffSingle from './user/userApplyDebuffSingle';
 import userApplyAttackSingle from './user/userApplyAttackSingle';
 import userApplyAttackAoE from './user/userApplyAttackAoE';
-import monstersApplyAttack from './monster/monstersApplyAttack';
 import userApplyRetliation from './user/userApplyRetaliation';
 import userApplyDebuffDamage from './user/userApplyDebuffDamage';
-import countDownBuffsAndDebuffs from './utils/countDownBuffsAndDebuffs';
 import userApplyBuffSingle from './user/userApplyBuffSingle';
-import selectAttack from './utils/selectAttack';
-import removeNewTagFromBuffsAndDebuffs from './utils/removeNewTagFromBuffsAndDebuffs';
 import userApplyPreBuffBattleChance from './user/userApplyPreBuffBattleChance';
-import applyEnemyDebuffEffects from './utils/applyEnemyDebuffEffects';
 import userApplyBattleCompleteEffects from './user/userApplyBattleCompleteEffects';
-import reFetchBattle from '../../helpers/fetchBattle';
-// TODO: Make code more readable by moving monster/user updates in their own designated function
-// TODO: APPLY BUFFS TO USER CHARACTER
+
+import countDownBuffsAndDebuffs from './utils/countDownBuffsAndDebuffs';
+import applyEnemyDebuffEffects from './utils/applyEnemyDebuffEffects';
+import removeNewTagFromBuffsAndDebuffs from './utils/removeNewTagFromBuffsAndDebuffs';
+import selectAttack from './utils/selectAttack';
+
+import monstersApplyAttack from './monster/monstersApplyAttack';
+
+import { calculateCharacterStats } from '../../helpers/stats/calculateCharacterStats';
+// import reFetchBattle from '../../helpers/fetchBattle';
+
 export const processBattleMove = async (
-  userCurrentCharacter,
-  battleOld,
-  currentSelectedMonster,
-  attackUsed,
-  io,
-  queue,
-  t,
+  userCurrentCharacter, // Our current character
+  battle, // The battle object
+  currentSelectedMonster, // The Monster we got selected
+  attackUsed, // The attack we used
+  t, // Sequelize Transaction object
 ) => {
-  const battle = await reFetchBattle(
-    battleOld,
-    t,
-  );
   let stageZeroInfoArray = []; // Start of Round effects (ex: stun from debuff)
   let stageOneInfoArray = []; // User Attacking Monsters
   let stageTwoInfoArray = []; // Monsters attack the user
@@ -41,199 +38,181 @@ export const processBattleMove = async (
   let stageSixInfoArray = []; // Stage 6 is a placeholder for after round effects
   let stageSevenInfoArray = []; // when battle is complete effects
   let isBattleComplete = false; // Test for battle completion
-  let totalDamageByMonsters = 0;
-  let totalHealedByLifeSteal = 0;
-  // let battleLogDatabasePromises = [];
-  let saveToDatabasePromises = [];
+  let totalDamageByMonsters = 0; // Total accumilated damage by monsters
+  let totalHealedByLifeSteal = 0; // total heal accumilated by healing effects
+  let saveToDatabasePromisesOne = []; // First database promises
+  let saveToDatabasePromisesTwo = []; // Second database promises (should not start to run before saveToDatabasePromisesOne has finished)
 
   // TODO: Only Gather Exp if Units are not lower then 5 levels of user level
   const sumExp = battle.BattleMonsters.reduce((accumulator, object) => accumulator + object.monster.exp, 0);
 
   const {
-    lvl,
-    attackOne,
-    attackTwo,
-    regularAttack,
-    block,
-    defense,
-    kick,
-    hp,
-    mp,
+    lvl, // The Users Current level
+    attackOne, // The user current Main Selected Attack
+    attackTwo, // The user current secondary selected Attack
+    regularAttack, // The user Regular attack
+    block, // The user Block
+    defense, // The user defense
+    kick, // the user kick attack
+    hp, // the user hp object
+    mp, // the user mp object
   } = await calculateCharacterStats(
     userCurrentCharacter,
   );
-  const useAttack = selectAttack(
-    userCurrentCharacter,
-    attackUsed,
-    attackOne,
-    attackTwo,
-    regularAttack,
-  );
-  await removeNewTagFromBuffsAndDebuffs(
-    userCurrentCharacter,
-    battle.BattleMonsters,
-    t,
-  );
 
-  const allBattleMonsters = await db.BattleMonster.findAll({
-    where: {
-      battleId: battle.id,
-    },
-    include: [
-      {
-        model: db.debuff,
-        as: 'debuffs',
-      },
-      {
-        model: db.monster,
-        as: 'monster',
-        include: [
-          {
-            model: db.monsterAttack,
-            as: 'monsterAttacks',
-            include: [
-              {
-                model: db.damageType,
-                as: 'damageType',
-              },
-            ],
-          },
-        ],
-      },
-    ],
-    lock: t.LOCK.UPDATE,
-    transaction: t,
-  });
-
+  // Create the current userState for processing
   let userState = JSON.parse(JSON.stringify(userCurrentCharacter));
   userState.hp = hp;
   userState.mp = mp;
   const initialUserState = JSON.parse(JSON.stringify(userState));
-  let battleMonsterState = JSON.parse(JSON.stringify(allBattleMonsters));
 
-  console.log('Processing Debuff Effects');
-  // APPLY ALL ENEMY DEBUFFS
+  // Parse all the battlemonsters into state array
+  let battleMonsterState = JSON.parse(JSON.stringify(battle.BattleMonsters));
 
+  // Test Which user attack to execute.. (pick regular attack if not enough mana)
+  // TODO: Throwing weapons?
+  const useAttack = selectAttack(
+    userState, // Pass Current user state
+    attackUsed, // Pass attack picked by user
+    attackOne, // The user current Main Selected Attack
+    attackTwo, // The user current secondary selected Attack
+    regularAttack, // The user Regular attack
+  );
+
+  console.log('Stage PreProcessing');
+  // Removing New Tag From Buffs/Debuffs
   [
-    battleMonsterState,
+    userState, // Return new user state
+    battleMonsterState, // Return new battlemonster state
+    saveToDatabasePromisesOne, // return database promises
+  ] = await removeNewTagFromBuffsAndDebuffs(
+    userState, // Pass userstate
+    battleMonsterState, // pass battlemonster state
+    saveToDatabasePromisesOne, // pass saveToDatabasePromisesOne array
+    t, // Sequelize Transaction object
+  );
+
+  /// APPLY ALL ENEMY DEBUFFS
+  [
+    battleMonsterState, // Return new battleMonsterState
   ] = await applyEnemyDebuffEffects(
-    battleMonsterState,
+    battleMonsterState, // Pass battleMonsterState
   );
 
   // DETERMINE IF UNIT IS STUNNED FROM DEBUFFS AND OTHER PREBATTLE DEBUFF EFFECTS
   console.log('Stage #0 Processing');
   [
-    stageZeroInfoArray,
-    userState,
-    battleMonsterState,
-    saveToDatabasePromises,
+    stageZeroInfoArray, // Return stageZeroInfoArray
+    userState, // Return new userstate
+    battleMonsterState, // Return new battlemonster state
+    saveToDatabasePromisesTwo, // Return database promises
   ] = await userApplyPreBuffBattleChance(
     userState, // Current User State
-    battleMonsterState,
+    battleMonsterState, // Pass battlemonster state
     stageZeroInfoArray, // Array to fill with battle info
     battle, // battle database record
     useAttack, // Which attack is used by user
     currentSelectedMonster.id, // which Monster do we have selected?
-    saveToDatabasePromises, // Database Promises to execute and wait for
+    saveToDatabasePromisesTwo, // Database Promises to execute and wait for
     t, // database transaction
   );
 
   console.log('Stage #1 Processing');
   // Stage One
   if (
-    useAttack.buff
-    && !useAttack.aoe
+    useAttack.buff // If attack is a buff
+    && !useAttack.aoe // and the attack is NOT an AOE attack
   ) {
     [
       stageOneInfoArray, // Return completed StageOneInfo Array
       userState, // Return new User State
-      saveToDatabasePromises, // Return battle promises
+      saveToDatabasePromisesTwo, // Return database promises
     ] = await userApplyBuffSingle(
       userState, // Current User State
       stageOneInfoArray, // Stage One Info Array
       battle, // passing battle object? (maybe we can reduce to just battle.id)?
       useAttack, // What attack are we using?
       currentSelectedMonster.id, // Which Monster is selected?
-      saveToDatabasePromises, // Saving to Database promises
+      saveToDatabasePromisesTwo, // pass the Database promises array
       t, // Transaction object sequelize
     );
   } else if (
-    useAttack.debuff
-    && useAttack.aoe
+    useAttack.debuff // if Attack is a debuff
+    && useAttack.aoe // and the attack is an AOE attack
   ) {
     [
       stageOneInfoArray, // Return completed StageOneInfo Array
-      userState,
-      battleMonsterState,
-      saveToDatabasePromises,
+      userState, // Return new userState
+      battleMonsterState, // Return new Battlemonster state
+      saveToDatabasePromisesTwo,
     ] = await userApplyDebuffAoE(
       userState, // Current User State
-      battleMonsterState,
-      stageOneInfoArray,
+      battleMonsterState, // Battlemonster state
+      stageOneInfoArray, // Pass stageOneInfoArray
       battle,
-      useAttack,
+      useAttack, // pass the user used attack
       currentSelectedMonster.id,
-      saveToDatabasePromises,
+      saveToDatabasePromisesTwo, // pass the Database promises array
       t,
     );
   } else if (
-    useAttack.debuff
-    && !useAttack.aoe
+    useAttack.debuff // If the attack is a debuff
+    && !useAttack.aoe // and not an AOE attack
   ) {
     [
       stageOneInfoArray, // Return completed StageOneInfo Array
-      userState,
-      battleMonsterState,
-      saveToDatabasePromises,
+      userState, // Return new userState
+      battleMonsterState, // Return new Battlemonster state
+      saveToDatabasePromisesTwo,
     ] = await userApplyDebuffSingle(
       userState, // Current User State
-      battleMonsterState,
-      stageOneInfoArray,
+      battleMonsterState, // pass the Battlemonster state
+      stageOneInfoArray, // pass StageOneInfo Array
       battle,
-      useAttack,
+      useAttack, // pass the user used attack
       currentSelectedMonster.id,
-      saveToDatabasePromises,
+      saveToDatabasePromisesTwo, // pass the Database promises array
       t,
     );
   } else if (
-    !useAttack.debuff
-    && useAttack.aoe
+    !useAttack.debuff // if the attack is NOT a debuff
+    && useAttack.aoe // and the attack is an AOE attack
   ) {
     [
       stageOneInfoArray, // Return completed StageOneInfo Array
-      userState,
-      battleMonsterState,
+      userState, // Return new userState
+      battleMonsterState, // Return new Battlemonster state
       totalHealedByLifeSteal,
-      saveToDatabasePromises,
+      saveToDatabasePromisesTwo,
     ] = await userApplyAttackAoE(
       userState, // Current User State
-      battleMonsterState,
+      battleMonsterState, // pass the Battlemonster state
       totalHealedByLifeSteal,
       lvl, // Users Level
       stageOneInfoArray, // Array to fill with battle info
       battle, // battle database record
       useAttack, // Which attack is used by user
       currentSelectedMonster.id, // which Monster do we have selected?
-      saveToDatabasePromises,
+      saveToDatabasePromisesTwo, // pass the Database promises array
       t, // database transaction
     );
   } else {
     [
       stageOneInfoArray, // Return completed StageOneInfo Array
-      userState,
-      battleMonsterState,
+      userState, // Return new userState
+      battleMonsterState, // Return new Battlemonster state
       totalHealedByLifeSteal,
-      saveToDatabasePromises,
+      saveToDatabasePromisesTwo,
     ] = await userApplyAttackSingle(
-      userState,
-      battleMonsterState,
-      totalHealedByLifeSteal,
+      userState, // pass the userState
+      battleMonsterState, // pass the battlemonster state
+      totalHealedByLifeSteal, // pass the totalHealedByLifeSteal variable
       lvl, // Users Level
       stageOneInfoArray, // Array to fill with battle info
       battle, // battle database record
       useAttack, // Which attack is used by user
       currentSelectedMonster.id, // which Monster do we have selected?
-      saveToDatabasePromises,
+      saveToDatabasePromisesTwo, // pass the Database promises array
       t, // database transaction
     );
   }
@@ -248,18 +227,19 @@ export const processBattleMove = async (
   console.log('Stage #2 Processing');
   // Process Monster Moves/Attacks
   if (!isBattleComplete) {
-    // if (battleMonsterState) {
+    // TODO: ADD ABILITY FOR ENEMY TO APPLY BUFFS TO SELF OR GROUP
+    // TODO: ADD ABILITY FOR ENEMY TO APPLY DEBUFFS TO USER
     [
-      totalDamageByMonsters,
-      userState,
-      battleMonsterState,
-      stageTwoInfoArray,
-      retaliationArray,
-      saveToDatabasePromises,
+      totalDamageByMonsters, // Return total damage done by monsters
+      userState, // Return the new userState
+      battleMonsterState, // Return new Battlemonster state
+      stageTwoInfoArray, // Return the stageTwoInfoArray to pass to render
+      retaliationArray, // Return array with retaliation info
+      saveToDatabasePromisesTwo, // DataPromises to execute
     ] = await monstersApplyAttack(
-      userState,
-      saveToDatabasePromises,
-      battleMonsterState,
+      userState, // Pass the userState
+      saveToDatabasePromisesTwo, // pass database promises array
+      battleMonsterState, // pass battlemonster state
       lvl, // Users Level
       block, // users Block
       defense, // Users defense
@@ -268,21 +248,20 @@ export const processBattleMove = async (
       battle, // battle database record
       t, // database transaction
     );
-    // }
 
     console.log('Stage #3 Processing');
     // Stage Three
     if (retaliationArray.length > 0) {
       [
-        stageThreeInfoArray,
-        userState,
-        battleMonsterState,
+        stageThreeInfoArray, // Return completed stageThreeInfoArray Array
+        userState, // Return the new userState
+        battleMonsterState, // Return new Battlemonster state
         totalHealedByLifeSteal,
-        saveToDatabasePromises,
+        saveToDatabasePromisesTwo,
       ] = await userApplyRetliation(
         userState,
         totalHealedByLifeSteal,
-        saveToDatabasePromises,
+        saveToDatabasePromisesTwo, // pass the Database promises array
         battleMonsterState,
         battle,
         retaliationArray,
@@ -297,9 +276,9 @@ export const processBattleMove = async (
     // Stage Four
     // Apply debuff damage
     [
-      stageFourInfoArray,
-      battleMonsterState,
-      userState,
+      stageFourInfoArray, // Return completed stageFourInfoArray Array
+      battleMonsterState, // Return new Battlemonster state
+      userState, // Return the new userState
     ] = await userApplyDebuffDamage(
       userState,
       battleMonsterState,
@@ -307,13 +286,17 @@ export const processBattleMove = async (
       stageFourInfoArray,
       t,
     );
+  }
 
+  await Promise.all(saveToDatabasePromisesOne);
+
+  if (!isBattleComplete) {
     // Stage 5
     // Count Down buffs, debuffs / after round effects (heal?)
     [
-      stageFiveInfoArray,
-      userState,
-      battleMonsterState,
+      stageFiveInfoArray, // Return completed stageFiveInfoArray Array
+      userState, // Return the new userState
+      battleMonsterState, // Return new Battlemonster state
     ] = await countDownBuffsAndDebuffs(
       stageFiveInfoArray,
       userState,
@@ -335,19 +318,21 @@ export const processBattleMove = async (
   // Stage 7 (Battle Complete effects) (Mana/Health REGEN)
   if (isBattleComplete) {
     [
-      stageSevenInfoArray,
-      userState,
+      stageSevenInfoArray, // Return completed stageSevenInfoArray Array
+      userState, // Return the new userState
       totalHealedByLifeSteal,
-      saveToDatabasePromises,
+      saveToDatabasePromisesTwo,
     ] = await userApplyBattleCompleteEffects(
       stageSevenInfoArray,
       userState,
       battle,
       totalHealedByLifeSteal,
-      saveToDatabasePromises,
+      saveToDatabasePromisesTwo, // pass the Database promises array
       t,
     );
-    saveToDatabasePromises.push(
+
+    // Tag the battle as complete
+    saveToDatabasePromisesTwo.push(
       new Promise((resolve, reject) => {
         db.battle.update({
           complete: true,
@@ -368,7 +353,7 @@ export const processBattleMove = async (
     - (totalDamageByMonsters - Math.round((totalDamageByMonsters * (userState.hp.totalLifeBonus / 100))))
     + (totalHealedByLifeSteal - Math.round((totalHealedByLifeSteal * (userState.hp.totalLifeBonus / 100))));
 
-  saveToDatabasePromises.push(
+  saveToDatabasePromisesTwo.push(
     new Promise((resolve, reject) => {
       userCurrentCharacter.condition.update({
         life: newLifeValue
@@ -384,7 +369,7 @@ export const processBattleMove = async (
   );
 
   for (const [i, battleMonsterStateToSave] of battleMonsterState.entries()) {
-    saveToDatabasePromises.push(
+    saveToDatabasePromisesTwo.push(
       new Promise((resolve, reject) => {
         db.BattleMonster.update({
           currentHp: battleMonsterStateToSave.currentHp,
@@ -399,7 +384,7 @@ export const processBattleMove = async (
     );
   }
 
-  await Promise.all(saveToDatabasePromises);
+  await Promise.all(saveToDatabasePromisesTwo);
 
   // Fetch Updated Battle for next round -> See battle controller for handling
   const updatedBattle = await db.battle.findOne({
